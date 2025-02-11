142c43120a4d643609fe0bde776b2e05d323feb7
Suhani <suhani.rai273@gmail.com>
2025-02-11 19:57:04 +0530

    Update transc.sv

--- transc.sv
+++ transc.sv
@@ -2,7 +2,7 @@ class transaction;
   bit newd;                   // Flag for new transaction
   rand bit [11:0] din;        // Random 12-bit data input
   bit [11:0] dout;            // 12-bit data output
- 
+  constraint din_less_than_150 { din>100; din<150; }    // Adding a constraint to streamline and control input values
   function transaction copy();
     copy = new();             // Create a copy of the transaction
     copy.newd = this.newd;    // Copy the newd flag
